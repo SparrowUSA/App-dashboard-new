d36e883be9ff5f77082e72d4a0933158bae89cd3
Sparrow <lol327970@gmail.com>
2026-01-17 19:57:26 +0530

    Update hyperlink_card.dart

--- lib/widgets/hyperlink_card.dart
+++ lib/widgets/hyperlink_card.dart
@@ -11,18 +11,14 @@ class HyperlinkCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 2,
       margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: ListTile(
-        leading: Icon(Icons.link, color: Colors.blue),
         title: InkWell(
-          onTap: () => launchUrl(Uri.parse(url)),
-          child: Text(
-            upwrite,
-            style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline, fontWeight: FontWeight.bold),
-          ),
+          onTap: () async => await launchUrl(Uri.parse(url)),
+          child: Text(upwrite, style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline, fontWeight: FontWeight.bold)),
         ),
-        subtitle: Text("Subject: $subject"),
+        subtitle: Text(subject),
+        trailing: Icon(Icons.open_in_new, size: 16),
       ),
     );
   }
